94da2e88119e743e02e3295bb7190ba7d2fd60c4
Lydia Koller <lydia@motel.is>
2020-02-06 11:37:36 -0600

    Return original string when noscrub option passed

--- lib/html_sanitize_ex/scrubber.ex
+++ lib/html_sanitize_ex/scrubber.ex
@@ -7,6 +7,11 @@ defmodule HtmlSanitizeEx.Scrubber do
     ""
   end
 
+  def scrub(html, HtmlSanitizeEx.Scrubber.NoScrub = scrubber_module) do
+    html
+    |> scrubber_module.before_scrub
+  end
+
   def scrub(html, scrubber_module) do
     html
     |> scrubber_module.before_scrub
